59c01d982508ebf4d371786924ab9358e4cb8a54
Padma Ganesh <ganeshpadma730@gmail.com>
2026-04-03 01:05:53 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost/bike-parking/backend/api';
+const API_URL = 'https://bikeparking.kesug.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
@@ -10,16 +10,16 @@ const api = axios.create({
 });
 
 export const parkVehicle = async (vehicle_no) => {
-  const response = await api.post('/entry.php', { vehicle_no });
+  const response = await api.post('/entry.php?i=1', { vehicle_no });
   return response.data;
 };
 
 export const exitVehicle = async (vehicle_no) => {
-  const response = await api.post('/exit.php', { vehicle_no });
+  const response = await api.post('/exit.php?i=1', { vehicle_no });
   return response.data;
 };
 
 export const getAllVehicles = async () => {
-  const response = await api.get('/vehicles.php');
+  const response = await api.get('/vehicles.php?i=1');
   return response.data;
 };
